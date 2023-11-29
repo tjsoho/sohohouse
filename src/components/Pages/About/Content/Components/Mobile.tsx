284885456c06fe9d95f24e4f91998b7d9b37ae78
MaxKeable <max.keable488@gmail.com>
2023-11-29 21:13:30 +1000

    fix:

--- src/components/Pages/About/Content/Components/Mobile.tsx
+++ src/components/Pages/About/Content/Components/Mobile.tsx
@@ -1,13 +1,12 @@
-
-
 interface ComponentProps {
   title: string;
   para1: string;
   para2: string;
   image: JSX.Element;
+  lineColour: string;
 }
-
-function Mobile({ title, para1, para2, image }: ComponentProps) {
+// You were trying to pass lineColour into this component but it was not on your interface.... I have a slight feeling you were getting confused with your Tablet.tsx component and thought that the props on that component were the ones going into this one... So in this component you actually arn't using lineColour.
+function Mobile({ title, para1, para2, image, lineColour }: ComponentProps) {
   return (
     <div className="py-24">
       <div className="px-8">
@@ -21,9 +20,7 @@ function Mobile({ title, para1, para2, image }: ComponentProps) {
           <p className="pb-4 text-Raleway text-brand-blue">{para1}</p>
         </div>
       </div>
-      <div>
-        {image}
-      </div>
+      <div>{image}</div>
       <div>
         <div className="px-8">
           <p className="pb-4 text-Raleway text-brand-blue">{para2}</p>
