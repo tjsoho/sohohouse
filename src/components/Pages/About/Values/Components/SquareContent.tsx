158d6d0adfa751f710a70114bbbe35369ce0d339
MaxKeable <max.keable488@gmail.com>
2023-12-03 16:48:52 +1000

    feat: :rocket:

--- src/components/Pages/About/Values/Components/SquareContent.tsx
+++ src/components/Pages/About/Values/Components/SquareContent.tsx
@@ -1,18 +1,13 @@
-
-
 interface SquareContentProps {
   content: string;
-  
 }
 
 function SquareContent({ content }: SquareContentProps) {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center py-[38px]">
       <p className="text-brand-orange text-center">{content}</p>
-      
     </div>
   );
 }
-
 
 export default SquareContent;
