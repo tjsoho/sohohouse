1650e271a8000b1ee05784d38b5ea2bfda5a7ae2
Tjcarroll1 <tjcarroll1@icloud.com>
2024-03-13 17:37:25 +1000

    added menu item

--- src/components/Menu/MenuPage.tsx
+++ src/components/Menu/MenuPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom"; // Import useLocation
-import { MenuItem } from "./MenuItem";
+import { useNavigate, useLocation } from "react-router-dom";
+import { MenuItem } from "./MenuItem"; // Ensure this import is correct
 
 interface MenuPageProps {
   toggleMenu: () => void;
@@ -8,22 +8,25 @@ interface MenuPageProps {
 
 const MenuPage: React.FC<MenuPageProps> = ({ toggleMenu }) => {
   const navigate = useNavigate();
-  const location = useLocation(); // Use the useLocation hook
+  const location = useLocation();
 
   const menuItems = [
     { text: "Home", route: "/", i: 1 },
-    { text: "About Me", route: "/about", i: 2 }, // Corrected the route
-    { text: "Ai HUB", route: "/aihub", i: 3 }, // Corrected the index
-    { text: "Websites", route: "/websites", i: 4 }, // Corrected the route and index
-    { text: "Portfolio", route: "/creations", i: 5 }, // Corrected the index
+    { text: "About Me", route: "/about", i: 2 },
+    { text: "Ai HUB", route: "/aihub", i: 3 },
+    { text: "Websites", route: "/websites", i: 4 },
+    { text: "Portfolio", route: "/creations", i: 5 },
     { text: "Member Login", route: "https://polite-rock-0e27c2600.5.azurestaticapps.net/", isExternal: true, i: 6 },
-    { text: "Let's Chat", route: "/contactme", i: 6 }, // Corrected the index
+    { text: "Let's Chat", route: "/contactme", i: 7 },
   ];
 
-  const handleItemClick = (route: string) => {
-    console.log("Item clicked, navigating to:", route);
+  const handleItemClick = (route: string, isExternal: boolean = false) => {
     toggleMenu();
-    navigate(route);
+    if (isExternal) {
+      window.open(route, "_blank");
+    } else {
+      navigate(route);
+    }
   };
 
   useEffect(() => {
@@ -34,14 +37,14 @@ const MenuPage: React.FC<MenuPageProps> = ({ toggleMenu }) => {
   }, []);
 
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen p-16 lg:p-32 bg-brand-blue flex flex-col justify-center items-center font-Black z-40 ">
+    <div className="fixed top-0 left-0 w-screen h-screen p-16 lg:p-32 bg-brand-blue flex flex-col justify-center items-center font-Black z-40">
       <ul style={{ listStyle: "none", padding: 0 }}>
         {menuItems.map((item) => (
           <MenuItem 
-            key={item.i} 
-            text={item.text} 
-            i={item.i} 
-            onClick={() => handleItemClick(item.route)}
+            key={item.i}
+            text={item.text}
+            i={item.i}
+            onClick={() => handleItemClick(item.route, item.isExternal)}
             isCurrentPage={location.pathname === item.route}
           />
         ))}
